bfb1cd29e56548f3fdb6f46d3863393bc29c694e
Kaycee Nalzaro <161752847+kyrr0o@users.noreply.github.com>
2024-04-15 11:28:56 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,95 +1,88 @@
 import tkinter as tk
 from tkinter import messagebox
 from student import StudentInformationSystemGUI
-from course import CourseInformationSystemGUI
+from course import CourseInformationSystemGUI  # Importing CourseInformationSystemGUI from course.py
 
 class SimpleStudentInformationSystem(tk.Tk):
     def __init__(self):
         super().__init__()
         self.title("Simple Student Information System")
         self.configure(bg="#DDDDDD")
-        self.geometry("650x350")
+        self.geometry("1400x700")
         self.resizable(False, False)
 
-        self.create_title_label()
         self.create_buttons()
-
-    def create_title_label(self):
-        title_label = tk.Label(
-            self,
-            text="STUDENT INFORMATION SYSTEM",
-            font=("Arial", 25, "bold"),
-            relief=tk.GROOVE,
-            bd=5,
-            bg="#2E2D2D",
-            foreground="white"
-        )
-        title_label.pack(side=tk.TOP, fill=tk.X)
-
-        registration_label = tk.Label(
-            self,
-            text="Choose Registration Option:",
-            font=("Arial", 15),
-            bg="#DDDDDD",
-            foreground="black"
-        )
-        registration_label.place(x=25, y=80)
+        self.create_student_registration_interface()
 
     def create_buttons(self):
-        student_button = tk.Button(
-            self,
-            text="STUDENT REGISTRATION",
-            font=("Arial", 15, "bold"),
-            bg="#2E2D2D",
-            foreground="white",
-            command=self.open_student_information
+        left_menu = tk.Frame(self, bg="#2E2D2D", bd=5, width=200, relief=tk.GROOVE)
+        left_menu.place(x=10, y=15, width=300, height=675)
+
+        menu_label = tk.Label(
+            left_menu,
+            text="MENU",
+            font=("Arial", 35, "bold"),
+            bg="gray",
+            fg="white"
         )
-        student_button.place(x=180, y=130, width=300)
+        menu_label.place(x=0, y=0, width=290)
+
+        text_label = tk.Label(
+            left_menu,
+            text="Choose Registration",
+            font=("Arial", 15),
+            bg="#2E2D2D",
+            fg="white",
+        )
+        text_label.place(x=50, y=100)
+
+        student_button = tk.Button(
+            left_menu,
+            text="STUDENT",
+            font=("Arial", 15, "bold"),
+            bg="gray",
+            foreground="white",
+            command=self.create_student_registration_interface
+        )
+        student_button.place(x=40, y=180, width=200)
 
         course_button = tk.Button(
-            self,
-            text="COURSE REGISTRATION",
+            left_menu,
+            text="COURSE",
             font=("Arial", 15, "bold"),
-            bg="#2E2D2D",
+            bg="gray",
             foreground="white",
-            command=self.open_course_information
+            command=self.create_course_registration_interface
         )
-        course_button.place(x=180, y=200, width=300)
+        course_button.place(x=40, y=240, width=200)
 
-        exit_btn = tk.Button(
-            self,
+        exit_button = tk.Button(
+            left_menu,
             text="EXIT",
             font=("Arial", 15, "bold"),
-            bg="#2E2D2D",
+            bg="gray",
             foreground="white",
             command=self.destroy
         )
-        exit_btn.place(x=180, y=270, width=300)
+        exit_button.place(x=40, y=580, width=200)
 
-    def open_student_information(self):
-        self.withdraw()
-        student_root = tk.Toplevel(self)
-        student_root.title("Student Registration")
-        student_root.configure(bg="#DDDDDD")
-        student_root.geometry("600x650") 
-        student_root.resizable(False, False)
-        student_root.protocol("WM_DELETE_WINDOW", lambda: self.close_window(student_root))
-        student_app = StudentInformationSystemGUI(student_root)
+    def create_student_registration_interface(self):
+        if hasattr(self, "student_interface_frame"):
+            self.student_interface_frame.destroy()
 
-    def open_course_information(self):
-        self.withdraw()
-        course_root = tk.Toplevel(self)
-        course_root.title("Course Information System")
-        course_root.configure(bg="#DDDDDD")
-        course_root.geometry("550x600") 
-        course_root.resizable(False, False) 
-        course_root.protocol("WM_DELETE_WINDOW", lambda: self.close_window(course_root))
-        course_app = CourseInformationSystemGUI(course_root)
+        self.student_interface_frame = tk.Frame(self, bg="#A6A6A6")
+        self.student_interface_frame.place(x=320, y=15, width=1070, height=675)
 
-    def close_window(self, window):
-        if messagebox.askokcancel("Quit", "Do you want to quit?"):
-            window.destroy()
-            self.deiconify()
+        self.student_app = StudentInformationSystemGUI(self.student_interface_frame)
+
+    def create_course_registration_interface(self):
+        if hasattr(self, "course_interface_frame"):
+            self.course_interface_frame.destroy()
+
+        self.course_interface_frame = tk.Frame(self, bg="#A6A6A6")
+        self.course_interface_frame.place(x=320, y=15, width=1070, height=675)
+
+        self.course_app = CourseInformationSystemGUI(self.course_interface_frame)
 
 def main():
     app = SimpleStudentInformationSystem()
